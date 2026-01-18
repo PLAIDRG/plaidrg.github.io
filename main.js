0d50edcfc3c1c102b366c503e81255f289207a79
Porter Squires <portersquires@gmail.com>
2026-01-17 20:45:36 -0500

    components

---
+++ main.js
@@ -0,0 +1,18 @@
+async function loadTemplate(url, templateId, targetId) {
+    const response = await fetch(url);
+    const html = await response.text();
+
+    // Parse fetched HTML
+    const container = document.createElement("div");
+    container.innerHTML = html;
+
+    const template = container.querySelector(`#${templateId}`);
+    const target = document.getElementById(targetId);
+
+    target.appendChild(template.content.cloneNode(true));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadTemplate("components/navbar.html", "navbar-template", "navbar");
+    loadTemplate("components/footer.html", "footer-template", "footer");
+});
